c97f99c9c12d1f54404c2eaa509623836f4437f3
dougmet <douglas.j.ashton@gmail.com>
2015-04-17 22:00:59 +0100

    Adding some R code

---
+++ R/processInput.R
@@ -0,0 +1,15 @@
+#' Process the input constraints
+#' 
+#' @param constraints A matrix or data frame containing the row and column sums
+#'
+#' @description This function checks and prepares the network constraints before
+#' calling the C++ code.
+#' 
+#' @return an object ready for sending to the C++ functions as input
+#' @author Douglas Ashton
+#' 
+processInput <- function(constraints) {
+  
+  
+  
+}
