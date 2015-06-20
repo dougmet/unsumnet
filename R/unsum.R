926de758258616d140b7648572c8715f41ddd869
dougmet <douglas.j.ashton@gmail.com>
2015-06-20 12:16:32 +0100

    Fixes #10 sorted out input names and written roxygen header.

--- R/unsum.R
+++ R/unsum.R
@@ -2,6 +2,33 @@
 #' 
 #' @param constraints A matrix or data frame containing the row and column sums.
 #' NA or negative values are interpretted as unconstrained.
+#' @param nEdges Integer The desired number of edges (non-zero elements) the 
+#' adjacency matrix should have.
+#' @param maxEdges Logical if set to TRUE then all possible edges will be used.
+#' The number of possible edges is given by \code{\link{calcMaxEdges}}. Default
+#' is FALSE.
+#' @param noReturn If set to TRUE then only one directed edge can go between two
+#' nodes. This would be appropriate if only modelling netted positions for example.
+#' Default is FALSE.
+#' @param mctSchedule Sets the number of MC loops between measurements. Also effects
+#' the quench speed as beta is only changed every mctSchedule loops.
+#' @param hotTime How many MC loops to run at the high temperature before starting
+#' the quench. The bigger the number the more time the network has to truly
+#' randomise before quenching.
+#' @param beta0 Starting inverse temperature. The smaller the number the hotter,
+#' or more random, the starting configuration is.
+#' @param betaMax The maximum beta value.
+#' @param mu0 The field that couples to the number of edges. The bigger this number
+#' the more strict the edge restriction is during the quench.
+#' @param coolingRate A number greater than, but close to, 1 that multiplies by \code{beta}
+#' every \code{mctSchedule} loops.
+#' @param maxTime The maximum number of loops before giving up on that quench and
+#' starting again.
+#' @param minError When the error drops below this threshold it is decided that it
+#' is close enough to a solution. The remaining error is resolved using the row/col
+#' iterator. A bigger number will give better performance but risks skewing the
+#' distirbution of networks.
+#'
 #' @description Given the row and column sums for a positive weighted
 #' adjacency matrix, this function will generate candidate matrices that are
 #' sampled uniformly from the space of possible matrices. This is done using
@@ -11,17 +38,17 @@
 #' @author Douglas Ashton
 #' @export
 unsum <- function(constraints,
-                  nedges=NULL,
+                  nEdges=NULL,
                   maxEdges=FALSE,
                   noReturn=FALSE,
-                  mct_schedule=100,
-                  hot_time=2000,
+                  mctSchedule=100,
+                  hotTime=2000,
                   beta0=1e-2,
-                  betamax=1e5,
+                  betaMax=1e5,
                   mu0=0.1,
-                  cooling_rate=1.02,
-                  max_time=1e6,
-                  cgmax=1e-3) {
+                  coolingRate=1.02,
+                  maxTime=1e6,
+                  minError=1e-3) {
   
   # Clean the input
   constraints <- processInput(constraints)
@@ -29,50 +56,52 @@ unsum <- function(constraints,
   # Quick length/type check of other inputs
   stopifnot(is.logical(maxEdges), length(maxEdges)==1)
   stopifnot(is.logical(noReturn), length(noReturn)==1)
-  stopifnot(is.numeric(mct_schedule), length(mct_schedule)==1)
-  stopifnot(is.numeric(hot_time), length(hot_time)==1)
+  stopifnot(is.numeric(mctSchedule), length(mctSchedule)==1)
+  stopifnot(is.numeric(hotTime), length(hotTime)==1)
   stopifnot(is.numeric(beta0), length(beta0)==1)
-  stopifnot(is.numeric(betamax), length(betamax)==1)
+  stopifnot(is.numeric(betaMax), length(betaMax)==1)
   stopifnot(is.numeric(mu0), length(mu0)==1)
-  stopifnot(is.numeric(cooling_rate), length(cooling_rate)==1)
-  stopifnot(is.numeric(max_time), length(max_time)==1)
-  stopifnot(is.numeric(cgmax), length(cgmax)==1)
+  stopifnot(is.numeric(coolingRate), length(coolingRate)==1)
+  stopifnot(is.numeric(maxTime), length(maxTime)==1)
+  stopifnot(is.numeric(minError), length(minError)==1)
   
   # I think something bad happens. Stop it.
   if(maxEdges & noReturn)
     stop("Can't have maxEdges=TRUE and noReturn=TRUE together")
   
-  if(!maxEdges & is.null(nedges))
-    stop("Must supply nedges or set maxEdges=TRUE")
+  if(!maxEdges & is.null(nEdges))
+    stop("Must supply nEdges or set maxEdges=TRUE")
   
-  if(!is.null(nedges)) {
-    stopifnot(is.numeric(nedges), length(nedges)==1)
-    if(nedges > nrow(constraints)*(nrow(constraints)-1))
+  if(!is.null(nEdges)) {
+    stopifnot(is.numeric(nEdges), length(nEdges)==1)
+    if(nEdges > nrow(constraints)*(nrow(constraints)-1))
       stop("Too many edges specified for matrix size")
     
-    if(nedges > calcMaxEdges(constraints[,1], constraints[,2])) {
+    if(nEdges > calcMaxEdges(constraints[,1], constraints[,2])) {
       warning("More edges specified than possible for these row/col sums")
     }
     
     if(maxEdges)
-      stop("Can't specify maxEdges and nedges together")
+      stop("Can't specify maxEdges and nEdges together")
   }
   
   # Specify the maximum number of edges if asked for
-  if(maxEdges) nedges <- calcMaxEdges(constraints[,1], constraints[,2])
+  if(maxEdges) nEdges <- calcMaxEdges(constraints[,1], constraints[,2])
+  
+  if(coolingRate<=1) stop("coolingRate must be greater than 1")
   
   unsumcpp(constraints,
-           nedges,
+           nEdges,
            maxEdges,
            noReturn,
-           mct_schedule,
-           hot_time,
+           mctSchedule,
+           hotTime,
            beta0,
-           betamax,
+           betaMax,
            mu0,
-           cooling_rate,
-           max_time,
-           cgmax)
+           coolingRate,
+           maxTime,
+           minError)
   
 }
 
@@ -85,6 +114,7 @@ unsum <- function(constraints,
 #' 
 #' @return maximum possible number of edges
 #' @author Douglas Ashton
+#' @export
 calcMaxEdges <- function(rs, cs) {
   n1 <- sum(rs>0)
   n2 <- sum(cs>0)
