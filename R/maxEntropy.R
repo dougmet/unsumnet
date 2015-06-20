6704a437861407c6861b0a14141e568fb9621970
dougmet <douglas.j.ashton@gmail.com>
2015-06-20 12:39:34 +0100

    Adding skeletons for maxEntropy R and Cpp #17. We'll use these to check for possibleness #11 and they are a useful package function

---
+++ R/maxEntropy.R
@@ -0,0 +1,12 @@
+
+#' Find the so-called maximum entropy solution 
+#'
+#' @param rs NumericVector the row sums of the matrix
+#' @param cs NumericVector the column sums of the matrix
+#'
+#' @return
+#' @export
+#'
+maxEntropy <- function(rs, cs) {
+  
+}
---
+++ src/maxEntropy.cpp
@@ -0,0 +1,10 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+NumericMatrix maxEntropyCpp(NumericVector rs, NumericVector cs) {
+  
+  
+}
+
