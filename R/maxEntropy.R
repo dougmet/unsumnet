f8accb273176f8da6ef8044a7c6081f2b168b032
dougmet <douglas.j.ashton@gmail.com>
2015-06-21 22:39:41 +0100

    Progress on #17 and #11 maxEntropy solution.

--- R/maxEntropy.R
+++ R/maxEntropy.R
@@ -9,4 +9,16 @@
 #'
 maxEntropy <- function(rs, cs) {
   
+  if (any(is.na(rs))) stop("maxEntropy can't yet cope with missing values in rs")
+  if (any(is.na(cs))) stop("maxEntropy can't yet cope with missing values in cs")
+  
+  if(length(rs)!=length(cs)) stop("rs and cs must be same length")
+  
+  if(sum(rs) != sum(cs)) stop("Sum of row and column sums should be the same")
+  
+  aw <- matrix(1, nrow=length(rs), ncol=length(cs)) - diag(1, nrow=length(rs))
+  
+  #err <- sum((csaw - cs)^2 + (rsaw - rs)^2)
+  
+  
 }
--- src/maxEntropy.cpp
+++ src/maxEntropy.cpp
@@ -3,8 +3,66 @@ using namespace Rcpp;
 
 
 // [[Rcpp::export]]
-NumericMatrix maxEntropyCpp(NumericVector rs, NumericVector cs) {
+NumericMatrix maxEntropyCpp(NumericMatrix aw, NumericVector rs, NumericVector cs) {
   
+  double err = 1;
+  int i=0;
+
+  int n = rs.length();
+  NumericVector rsaw(n);
+  NumericVector csaw(n);
+  
+  LogicalVector rson(n, true);
+  LogicalVector cson(n, true);
+  
+  for (int i=0; i<n; i++) {
+    if(rs[i] > 1e-12) rson[i] = false;
+    if(cs[i] > 1e-12) cson[i] = false;
+  }
+    
+  while(err > 1e-18) {
+    // Get the row sums
+    for(int i=0; i<n; i++) {
+      rsaw[i]=0;
+      if (rson[i]) {
+        for (int j=0;j<n;j++) {
+          rsaw[i] += aw(i,j);
+        }
+      } else {
+        rsaw[i]=1;
+      }
+    }
+    
+    // Apply row sums
+    for (int i=0;i<n;i++) {
+      for (int j=0;j<n;j++) {
+        aw(i,j) *= rs[i] / rsaw[i];
+      }
+    }
+    
+    // Now for columns
+    // Get the col sums
+    for(int i=0; i<n; i++) {
+      csaw[i]=0;
+      if (cson[i]) {
+        for (int j=0;j<n;j++) {
+          csaw[i] += aw(j,i);
+        }
+      } else {
+        csaw[i]=1;
+      }
+    }
+    
+    // Apply col sums
+    for (int i=0;i<n;i++) {
+      for (int j=0;j<n;j++) {
+        aw(j,i) *= cs[i] / csaw[i];
+      }
+    }
+    
+    
+    
+  }
   
 }
 
