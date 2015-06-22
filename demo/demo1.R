23c5954b00ad9edfd06401533fe29b7e78f8d9fd
dougmet <douglas.j.ashton@gmail.com>
2015-06-22 23:28:29 +0100

    Added a bit to the demo, soon to be vignette

--- demo/demo1.R
+++ demo/demo1.R
@@ -9,3 +9,27 @@ library(igraph)
 g <- graph.adjacency(fit$AW, weighted = TRUE)
 
 plot(g, edge.width=E(g)$weight, edge.curved=is.mutual(g))
+
+
+# This is a neat example where maxEntropy is rubbish.
+set.seed(33)
+a <- matrix(sample(0:1, 100, prob = c(0.85,0.15), replace=TRUE), ncol=10)
+diag(a) <- 0
+awTrue <- a * abs(rnorm(100, 40, 15))
+
+rs <- rowSums(awTrue)
+cs <- colSums(awTrue)
+
+awME <- maxEntropy(rs,cs)
+View(awTrue)
+
+hist(awME, freq=FALSE)
+hist(awTrue, freq=FALSE)
+
+gTrue <- graph.adjacency(awTrue, weighted = TRUE)
+plot(gTrue)
+
+gME <- graph.adjacency(awME, weighted = TRUE)
+plot(gME)
+
+
