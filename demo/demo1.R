e0d56dc50003bb65fba3c462898de3e8ad46404b
dougmet <douglas.j.ashton@gmail.com>
2015-06-28 22:47:08 +0100

    Skeleton vignette going in #20

--- demo/demo1.R
+++ demo/demo1.R
@@ -1,12 +1,10 @@
 ## TO BE CONVERTED TO VIGNETTE
 
-set.seed(10)
+set.seed(11)
 constraints <- matrix(rep(c(10,6,6,4,2,1),2),ncol=2)
 fit <- unsum(constraints, 12)
 
 library(igraph)
-
-
 g <- graph.adjacency(fit$AW, weighted = TRUE)
 
 plot(g, edge.width=E(g)$weight, edge.curved=is.mutual(g))
