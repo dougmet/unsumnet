72217cbd15395c13fe00d8c7c530934faab9f9d7
dougmet <douglas.j.ashton@gmail.com>
2015-06-30 23:07:38 +0100

    Fixes #26 nettedMatrix added S3 generic.

--- R/nettedMatrix.R
+++ R/nettedMatrix.R
@@ -8,7 +8,19 @@
 #' @return A numeric matrix with the positive elements of w-w'
 #' @export
 #'
-nettedMatrix <- function(w) {
+nettedMatrix <- function(w) UseMethod("nettedMatrix")
+
+#' @title Calculate the netted matrix (default)
+#'
+#' @description Subtract the transpose and keep positive elements of a weighted
+#' adjacency matrix.
+#'
+#' @param w A square numeric matrix
+#'
+#' @return A numeric matrix with the positive elements of w-w'
+#' @export
+#'
+nettedMatrix.default <- function(w) {
   
   if(!is.matrix(w) | !is.numeric(w)) stop("Must be a numeric matrix")
   if(nrow(w) != ncol(w)) stop("Must be square matrix")
@@ -16,4 +28,18 @@ nettedMatrix <- function(w) {
   wOut <- w - t(w)
   
   return(wOut * (wOut>0))
+}
+
+#' @title Calculate the netted matrix (default)
+#'
+#' @description Subtract the transpose and keep positive elements of a weighted
+#' adjacency matrix.
+#'
+#' @param x An \code{unsumnet} object usually from \code{\link{unsum}}.
+#'
+#' @return A numeric matrix with the positive elements of aw-aw'
+#' @export
+#'
+nettedMatrix.unsumnet <- function(x) {
+  nettedMatrix.default(x$AW)
 }
