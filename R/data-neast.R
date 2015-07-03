769e5639ad9ed26e9b715d909e3cf7821f8de468
dougmet <douglas.j.ashton@gmail.com>
2015-07-03 23:14:36 +0100

    Swapped data from .RData to .rda

---
+++ R/data-neast.R
@@ -0,0 +1,10 @@
+#' North East Bank data
+#' 
+#' @description A demo balance sheet for six fictional banks
+#' @docType data
+#' @name neast
+#' @usage neast
+#' @format A \code{\link{data.frame}} with 6 observations.
+#' @examples
+#' neast
+NULL
