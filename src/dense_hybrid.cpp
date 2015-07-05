71048c763d52cf32065c12f4726e2e6b38374c22
dougmet <douglas.j.ashton@gmail.com>
2015-07-05 21:15:26 +0100

    Adding license specifically to the C++ code

--- src/dense_hybrid.cpp
+++ src/dense_hybrid.cpp
@@ -1,3 +1,22 @@
+/*
+ 
+ This file is part of dense_hybrid.
+ 
+ dense_hybrid is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ dense_hybrid is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
 #include "dense_hybrid.h"
 
 dense_hybrid::dense_hybrid(int nn_in, int target_ne_in,
