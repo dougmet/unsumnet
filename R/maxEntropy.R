23d4c3ac8694bc63a0ccfaec97865d27bef82c48
dougmet <douglas.j.ashton@gmail.com>
2015-07-06 21:04:37 +0100

    Down to 3 #21

--- R/maxEntropy.R
+++ R/maxEntropy.R
@@ -16,6 +16,7 @@
 #' @param cs NumericVector the column sums of the matrix.
 #' @param minError Numeric. The algorithm will keep iterating until the mean squared 
 #' error against the constraints drops below this value.
+#' @param ... extra arguments passed to \code{\link{maxEntropy.numeric}}
 #'
 #' @return A matrix that satisfies the row and column sum constraints or FALSE
 #'  if the algorithm failed to converge. Dimension names will be pulled through
